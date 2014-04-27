c6a4f438ac7c74b33e34c7b14f748755e9825ad5
kaz-yos <kazky@mac.com>
2014-04-27 14:27:13 -0400

    atm example

---
+++ 2014-04-27.atm.py
@@ -0,0 +1,21 @@
+###
+### example program: ATM
+## Create variables
+user_pin = 5544
+user_balance = 500
+
+## define an ATM
+pin_attempt = input("enter your PIN ")
+pin_attempt = int(pin_attempt)  # convert to int
+
+if pin_attempt == user_pin:
+    amount_to_withdraw = int(input("How much would youlike to withdraw? "))
+    if amount_to_withdraw <= user_balance:
+        print("Disbursing " + str(amount_to_withdraw))
+        print("Remaining balance: " + str(user_balance))
+    else:
+        print("Insufficient funds.")
+else:
+    print("Invalid PIN.")
+
+print("Transaction concluded.")
