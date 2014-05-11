5904275bd28086e885542b488eb7c26e1150de5c
kaz-yos <kazky@mac.com>
2014-05-11 13:17:06 -0400

    added 2014-05-11

---
+++ 2014-05-11.beginner.py
@@ -0,0 +1,39 @@
+### 2014-05-11 Learning to program with Python
+## Lists
+
+## Create an empty list
+myList = []
+myList
+
+myList = ["h", True, 4, 6, "string"]
+myList
+
+## Lists are dynamic containers
+myList = []
+len(myList)
+
+myList = [1, "a", 6]
+len(myList)
+myList.append("hi")
+myList
+
+myList = [1,2,3]
+myList
+myList.count(1)
+
+myList.pop()
+myList
+myList.pop()
+myList
+myList.pop()
+myList
+
+
+myList = [5,1,2,3,4]
+myList
+myList.sort()
+myList
+
+
+m = [1,2,3,4,5]
+type(m)
