a7204cf019b0360c5061a6f2f851f78bf9458c06
kaz-yos <kazky@mac.com>
2014-06-08 13:18:10 -0400

    Namespace variables and a function

--- 2014-06-08.beginner.py
+++ 2014-06-08.beginner.py
@@ -1,3 +1,9 @@
 ### 2014-06-08 Python meetup beginner
 
+import stuff
 
+print(stuff.number)
+print(stuff.age)
+print(stuff.rate)
+
+print(stuff.capitalizeWords("hello I am a hoser."))
--- stuff.py
+++ stuff.py
@@ -1,18 +1,9 @@
-## No name. When does it happen? It will be run at the time of import.
-if 3 + 3 == 6:
-    for n in range(10):
-        print("hey")
-
-## Define         
-pi = 3.14
-
-def add(x,y):
-    return x + y
-
+## Define variables
 number = 5
-age = 10
-rate = 15
+age    = 10
+rate   = 15
 
+## Define a function
 def capitalizeWords(string):
     words = string.split()
     result = []
