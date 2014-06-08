2e4c6cf18de35efe67a6559efb35aa813ba0c969
kaz-yos <kazky@mac.com>
2014-06-08 15:49:33 -0400

    Add move method

--- 2014-06-08.advanced.py
+++ 2014-06-08.advanced.py
@@ -6,17 +6,29 @@
 
 ## Inherit from the object class (prototype of all objects)
 class Monster(object):
+    ## Monster class attribute
+    ## No "self" to mention the instance
+    counter = 0
+    ##
     def __init__(self):
+        Monster.counter += 1
         self.health       = 100
         self.attack       = 20
         self.defense      = 20
         self.speed        = 10
         self.accuracy     = 0.5
         self.magic_resist = 0.3
+        self.x            = 0
+        self.y            = 0
+    def move(self, x, y):
+        self.x += x
+        self.y += y
 
+class Demon:
+    pass
 
 ## Inheritance
-class Banshee(Monster):
+class Banshee(Monster, Demon):
     ## Overwrite the default __init__j
     def __init__(self):
         ## Monster's __init__ method is invoked
@@ -34,6 +46,10 @@ class Dragon(Monster):
         self.attack  = 200
         self.defense = 200
         self.health  = 1000
+    def move(self,x,y):
+        # self.x += 5 * x
+        # self.y += 5 * y
+        super.move(5 * x, 5 * y)
 
 
 ##
